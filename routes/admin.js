8091b3ec53d3eb0e5c5c86a658d8e360e93187df
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 15:18:22 +0100

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
+const path = require("path");
+
 const router = express.Router();
 
 //only for get request
 router.get("/add-product", (req, res, next) => {
   //   console.log("In another middleweare!");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 //only for POST request
