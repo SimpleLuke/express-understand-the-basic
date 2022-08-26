a938ac81b9f2c213e7273ae0f49237ad9e4a0ac4
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 13:57:47 +0100

    Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.use("/", (req, res, next) => {
-  console.log("This alwasy run");
+  //   console.log("This alwasy run");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleweare!");
-  res.send("<h1>Add Product Page</h1>");
+  //   console.log("In another middleweare!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 app.use("/", (req, res, next) => {
-  console.log("In another middleweare!");
+  //   console.log("In another middleweare!");
   res.send("<h1>Hello world!</h1>");
 });
 
