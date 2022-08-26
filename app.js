98fc0e91661a80f90cbe7ad138143e19e32c8b28
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 16:02:40 +0100

    Serving Files Statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const shopRoutes = require("./routes/home");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
