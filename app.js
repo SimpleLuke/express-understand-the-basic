c756f800022ecabe4d11f1868c2b2001e7c36b8d
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 14:19:29 +0100

    Using Express Router

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/home");
 
 const app = express();
 
@@ -8,25 +10,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.use("/", (req, res, next) => {
-  //   console.log("This alwasy run");
-  next();
-});
+// app.use("/", (req, res, next) => {
+//     console.log("This alwasy run");
+//   next();
+// });
 
-app.use("/add-product", (req, res, next) => {
-  //   console.log("In another middleweare!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  //   console.log("In another middleweare!");
-  res.send("<h1>Hello world!</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
