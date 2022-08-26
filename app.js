ce5cfd3527151db88412e830429e6cc1b638952c
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 09:16:11 +0100

    How Middleware Works

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
   console.log("In another middleweare!");
+  res.send("<h1>Hello world!</h1>");
 });
 
 const server = http.createServer(app);
