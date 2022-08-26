cd51afbabecce0b2832a7e7a0fd9c9e6b56bdcea
Luke Lai <lukelai.dev@gmail.com>
2022-08-26 09:28:31 +0100

    Handling Different Routes

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const app = express();
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.use((req, res, next) => {
-  console.log("In the middleweare!");
+app.use("/", (req, res, next) => {
+  console.log("This alwasy run");
   next();
 });
-app.use((req, res, next) => {
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleweare!");
+  res.send("<h1>Add Product Page</h1>");
+});
+app.use("/", (req, res, next) => {
   console.log("In another middleweare!");
   res.send("<h1>Hello world!</h1>");
 });
